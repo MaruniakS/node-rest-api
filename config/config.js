83e03748a27f6584e8896593a23f7695c2b5e104
Stanislav Maruniak <stanislav.maruniak@kindgeek.com>
2018-11-07 14:46:20 +0200

    add dotenv

---
+++ config/config.js
@@ -0,0 +1,31 @@
+var dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.load();
+}
+
+module.exports = {
+  development: {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  test: {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL'
+  }
+};
