703df8c9ec77dd6e1d6bcbd2304743ebfaa6e2c9
Stanislav Maruniak <stanislav.maruniak@kindgeek.com>
2018-11-09 18:50:18 +0200

    user model added

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      required: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      required: true,
+      validate: {
+        len: { args: [5, 25], msg: 'Username should contain 5-25 symbols.' },
+        notEmpty: true
+      }
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
