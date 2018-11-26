2d4957952364e9282cee8f2cf087020f01142d6f
maruniaks <st.maruniak@gmail.com>
2018-11-26 23:10:54 +0200

    fix

--- models/index.js
+++ models/index.js
@@ -10,8 +10,10 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log('here1', process.env[config.use_env_variable]);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  console.log('HERE 2');
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
